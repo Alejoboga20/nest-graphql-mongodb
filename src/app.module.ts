be6c7bc57f7df6b867558cbb4a508def97e3fccb
Alejo Boga <alejoboga20@gmail.com>
2022-06-10 10:35:02 -0300

    Create lesson module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
+import { LessonModule } from './lesson/lesson.module';
+
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
+    LessonModule,
   ],
 })
 export class AppModule {}
