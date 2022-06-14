31682225ca444bbf2d25cf0aa2fc013bd7b39edf
Alejo Boga <alejoboga20@gmail.com>
2022-06-14 10:01:00 -0300

    Student mutation

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+import { CreateStudentInput } from './create-student.input';
+import { Student } from './student.entity';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectRepository(Student) private studentRepository: Repository<Student>,
+  ) {}
+
+  async createStudent(
+    createStudentInput: CreateStudentInput,
+  ): Promise<Student> {
+    const student = this.studentRepository.create({
+      id: uuid(),
+      ...createStudentInput,
+    });
+
+    return await this.studentRepository.save(student);
+  }
+}
